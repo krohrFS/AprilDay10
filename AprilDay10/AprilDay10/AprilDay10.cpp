6dd79e8f83acb27e43fedbe9572826f5aa197bc3
Rohr <krohr@fullsail.com>
2026-04-27 14:07:13 -0400

    This is our first commit

---
+++ AprilDay10/AprilDay10/AprilDay10.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+/*
+    Karl Rohr   
+    Contact Info
+        Email: krohr@fullsail.edu
+        Discord: karlrohr
+
+
+    Midterm and lab 6 will start being graded tomorrow evening. Remember that the midterm can not be turned in late. It is better
+    to turn something in before the deadline than nothing at all.
+
+    Going over instructions and flow of the Midterm
+
+
+    Tasks for the day
+        Any review/questions/examples people want to see
+
+
+    // Begin of stuff for PG2 and beyond
+    Git with Github
+   Make an account with student email https://github.com/
+   Download this  https://git-scm.com/install/windows
+
+    Lots of command line
+
+    File I/O
+        Writing to a text File
+        Reading from a text File
+
+    Changing font color
+    // End of stuff for PG2 and beyond
+    
+    Game demo stuff
+
+    We'll get started at 1:15pm est
+
+
+*/
+int main()
+{
+    std::cout << "Hello World!\n";
+}
